b98ec608c0cd0c209334059069693ef55551a160
Tiago Silva <tiago.arasilva@gmail.com>
2025-02-26 16:20:23 +0100

    Add new shortcuts

--- edgy_guardian/shortcuts.py
+++ edgy_guardian/shortcuts.py
@@ -107,7 +107,9 @@ async def assign_group_perm(
     )
 
 
-async def assign_perm(perm: str, users: Any, obj: Any | None = None, revoke: bool = False) -> None:
+async def assign_perm(
+    perm: type[edgy.Model] | str, users: Any, obj: Any | None = None, revoke: bool = False
+) -> None:
     """
     Assigns or revokes a permission for a user or group on a specific object.
 
@@ -116,7 +118,7 @@ async def assign_perm(perm: str, users: Any, obj: Any | None = None, revoke: boo
     If `obj` is None, the permission is assigned or revoked globally.
 
     Args:
-        perm (str): The permission to assign or revoke. This should be a string representing the permission name.
+        perm (type[edgy.Model] | str): The permission to assign or revoke. This should be a string representing the permission name.
         users (Any): The user or group to assign or revoke the permission for. This can be an instance of a User or Group model.
         obj (Any, optional): The object to assign or revoke the permission for. This can be any object for which permissions are managed. Defaults to None, meaning the permission is assigned or revoked globally.
         revoke (bool, optional): If True, the permission will be revoked instead of assigned. Defaults to False.
@@ -140,4 +142,72 @@ async def assign_perm(perm: str, users: Any, obj: Any | None = None, revoke: boo
         obj=obj,
         perm=perm,
         revoke=revoke,
+    )
+
+
+async def remove_perm(perm: type[edgy.Model] | str, users: Any, obj: Any | None = None) -> None:
+    """
+    Removes a permission from a user or group for a specific object.
+
+    This asynchronous function revokes a specified permission from a user or group.
+    If the `obj` parameter is provided, the permission is revoked for that specific object.
+    If `obj` is None, the permission is revoked globally.
+
+    Args:
+        perm (type[edgy.Model] | str): The permission to revoke. This should be a string representing the permission name or a permission model instance.
+        users (Any): The user or group from whom the permission will be revoked. This can be an instance of a User or Group model.
+        obj (Any, optional): The object for which the permission is being revoked. This can be any object for which permissions are managed. Defaults to None, meaning the permission is revoked globally.
+
+    Returns:
+        None: This function does not return any value.
+
+    Example:
+        # Revoke the 'edit' permission from a user for a specific object
+        await remove_perm('edit', user_instance, obj=some_object)
+
+        # Revoke the 'delete' permission from a group globally
+        await remove_perm('delete', group_instance)
+    """
+    return await assign_perm(perm, users, obj, revoke=True)
+
+
+async def remove_group_perm(
+    perm: type[edgy.Model] | str,
+    group: type[edgy.Model] | str,
+    users: type[edgy.Model] | None = None,
+    obj: Any | None = None,
+    revoke_users_permissions: bool = False,
+) -> None:
+    """
+    Removes a permission from a group.
+
+    This asynchronous function revokes a specified permission from a group.
+    It can also optionally revoke the permission for specific users within the group
+    and/or for a specific object. If the `revoke_users_permissions` parameter is set to True,
+    the permission will also be revoked from the users within the group.
+
+    Args:
+        perm (type[edgy.Model] | str): The permission to revoke. This should be a string representing the permission name or a permission model instance.
+        group (type[edgy.Model] | str): The group from which the permission will be revoked. This can be an instance of the group model or the name of the group.
+        users (type[edgy.Model] | None, optional): The users within the group for whom the permission will be revoked. Defaults to None.
+        obj (Any | None, optional): The object for which the permission is being revoked. Defaults to None.
+        revoke_users_permissions (bool, optional): If set to True, the permission will also be revoked from the users within the group. Defaults to False.
+
+    Returns:
+        None: This function does not return any value.
+
+    Example:
+        # Revoke the 'edit' permission from a group
+        await remove_group_perm('edit', group_instance)
+
+        # Revoke the 'view' permission from a group and also from the users within the group
+        await remove_group_perm('view', group_instance, revoke_users_permissions=True)
+    """
+    return await assign_group_perm(
+        perm=perm,
+        group=group,
+        users=users,
+        obj=obj,
+        revoke=True,
+        revoke_users_permissions=revoke_users_permissions,
     )
